7fbf7b1b7492cad6fed6a13b5605604775488947
leixingyu <techartlei@gmail.com>
2022-06-22 15:39:50 -0400

    add function

--- common/debug.py
+++ common/debug.py
@@ -15,3 +15,15 @@ def pprint_attr_shortname(mobject):
 
     for i in range(len(longs)):
         print('{}: {}'.format(longs[i], shorts[i]))
+
+
+def get_loaded_plugins(is_auto=False):
+    """
+    Get loaded plugin in current maya session
+
+    :param is_auto: bool. filter auto loaded plugins
+    :return: list. full path of the plugin
+    """
+    plugins = cmds.pluginInfo(query=1, listPlugins=1, autoload=is_auto)
+    return [cmds.pluginInfo(p, query=1, path=1) for p in plugins]
+
